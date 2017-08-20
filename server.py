b89d450e3dccfa531d76e3b0f8df84ad0f41fa87
Soriga Andrei <andrei.soriga@gmail.com>
2017-08-20 18:27:52 +0300

    Improved file upload route and added a route for form-data

--- server.py
+++ server.py
@@ -20,17 +20,35 @@ configure_uploads(app, upload_files)
 
 @app.route('/uploads', methods=['POST'])
 def upload():
+
     if 'file' in request.files:
-        if 'file' in request.files:
+
+        filenames = []
+
+        for file_request in request.files.getlist("file"):
             try:
-                filename = upload_files.save(request.files['file'])
-                return jsonify({'filename': filename})
+                filename = upload_files.save(file_request)
+                filenames.append(filename)
             except UploadNotAllowed:
                 print('Given extension is not in the allowed list.')
                 # status codes http://www.restapitutorial.com/httpstatuscodes.html
                 return jsonify({"error": 'Given extension is not in the allowed list'}), 415
 
-        return jsonify('No file found in the request'), 400
+        return jsonify({'filenames': filenames})
+
+    return jsonify({'state': 'No file found in the request'}), 400
+
+
+@app.route('/form-data', methods=['POST'])
+def form_data():
+
+    params = request.get_json()
+
+    if 'filenames' not in params or not params['filenames']:
+        return jsonify('Filename must be provided'), 406
+
+    return jsonify(params)
+
 
 manager = Manager(app)
 
